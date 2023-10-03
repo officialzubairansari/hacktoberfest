40e70c4792e9d3ffe4605182ccaa599ec83cc3f9
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-10-03 10:25:59 +0530

    Added Hello World in C++

---
+++ scripts/hello_world_dhruv.cpp
@@ -0,0 +1,11 @@
+// Language: C++
+// Env: C++17
+// Author: Dhruv Chandak
+// GitHub: https://github.com/dhruvchandak30
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
